aaba7b27eef7dd622ce4016c191a84339323ae0e
Takuya Hayakawa <Takuya.Hayakawa@sony.com>
2017-08-01 19:07:29 +0900

    Create a scene to test to save/load customize data.
    Implement save button.

---
+++ Assets/Scripts/Button_CustomizeStateSave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_CustomizeStateSave : MonoBehaviour {
+
+    public GameObject CustomizeStatusObject;
+
+    public void OnPressedSaveButton()
+    {
+        if(this.CustomizeStatusObject == null)
+        {
+            Debug.LogError("CustomizeStatusObject instance is not set.");
+            return;
+        }
+
+        var state = this.CustomizeStatusObject.GetComponent<AvatarCustomizeStatus>();
+        if(state == null)
+        {
+            Debug.LogError("CustomizeStatusObject doesn't have a component AvatarCustomizeStatus");
+            return;
+        }
+
+        string json = JsonUtility.ToJson(state, true);
+        Debug.LogFormat("Customize Status: {0}", json);
+    }
+}
