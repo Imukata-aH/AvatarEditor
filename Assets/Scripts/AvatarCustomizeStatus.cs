d76d8b9cf7f086f3c469a6b3643559d4d35c10f4
Imukata-aH <leahyleahyleahy@gmail.com>
2017-08-08 20:22:59 +0900

    Add asset data.
    Add customize button implementation stub.

--- Assets/Scripts/AvatarCustomizeStatus.cs
+++ Assets/Scripts/AvatarCustomizeStatus.cs
@@ -5,30 +5,16 @@ using UnityEngine;
 public enum CustomizePartsType
 {
     TopsModel,
-    TopsMaterial,
     BottomsModel,
-    BottomsMaterial,
     ShoesModel,
-    ShoesMaterial,
-    HairModel,
-    HairColor,
-    EyeTexture,
-    SkinColor,
 }
 
 [System.Serializable]
 public class AvatarCustomizeStatus
 {
     public int TopsModel;
-    public int TopsMaterial;
     public int BottomsModel;
-    public int BottomsMaterial;
     public int ShoesModel;
-    public int ShoesMaterial;
-    public int HairModel;
-    public Color HairColor;
-    public int EyeTexture;
-    public Color SkinColor;
 
     public void SetCustomizeState(CustomizePartsType type, int index)
     {
@@ -37,41 +23,11 @@ public class AvatarCustomizeStatus
             case CustomizePartsType.TopsModel:
                 this.TopsModel = index;
                 break;
-            case CustomizePartsType.TopsMaterial:
-                this.TopsMaterial = index;
-                break;
             case CustomizePartsType.BottomsModel:
                 this.BottomsModel = index;
                 break;
-            case CustomizePartsType.BottomsMaterial:
-                this.BottomsMaterial = index;
-                break;
             case CustomizePartsType.ShoesModel:
                 this.ShoesModel = index;
-                break;
-            case CustomizePartsType.ShoesMaterial:
-                this.ShoesMaterial = index;
-                break;
-            case CustomizePartsType.HairModel:
-                this.HairModel = index;
-                break;
-            case CustomizePartsType.EyeTexture:
-                this.EyeTexture = index;
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void SetCustomizeState(CustomizePartsType type, Color color)
-    {
-        switch (type)
-        {
-            case CustomizePartsType.HairColor:
-                this.HairColor = color;
-                break;
-            case CustomizePartsType.SkinColor:
-                this.SkinColor = color;
                 break;
             default:
                 break;
---
+++ Assets/Scripts/Button_CustomizeAvatar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_CustomizeAvatar : MonoBehaviour {
+
+    public uint AssetID
+    {
+        get;
+        private set;
+    }
+
+    public void LoadTargetAssetData(uint assetIndex)
+    {
+        this.AssetID = assetIndex;
+
+        AssetMetaData metadata = LoadAssetMetadata(assetIndex);
+        Sprite assetImage = LoadAssetImage(assetIndex);
+        Image assetImageBox = this.GetComponent<Image>();
+        assetImageBox.sprite = assetImage;
+
+        string assetName = metadata.AssetName;
+        Text assetNameBox = this.GetComponent<Text>();
+        assetNameBox.text = assetName;
+    }
+
+    private AssetMetaData LoadAssetMetadata(uint assetIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Sprite LoadAssetImage(uint assetIndex)
+    {
+        throw new NotImplementedException();
+    }
+}
